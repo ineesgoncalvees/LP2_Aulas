77ce823b4092e8d2c420dfbe381895b3b051418a
ineesgoncalvees <ines98goncalves@gmail.com>
2018-10-02 12:01:11 +0100

    Adicionar metodos

---
+++ Aula1-fortnite/Aula1-fortnite/FHPlayer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1_fortnite
+{
+    class FHPlayer
+    {
+        
+        public string equipedWeapon;
+        public double hitPoints;
+        public double shield;
+
+        public FHPlayer()
+        {
+            equipedWeapon = "picareta";
+            hitPoints = 100;
+            shield = 0;
+        }
+
+        public FHPlayer(string equipedWeapon, double hitPoints, double shield)
+        {
+            this.equipedWeapon = equipedWeapon;
+            this.hitPoints = hitPoints;
+            this.shield = shield;
+        }
+
+        public void Attack(FHPlayer enemy)
+        {
+            enemy.TakeDamage(10);
+        }
+
+        public void TakeDamage(double damage)
+        {
+            if(shield > 0)
+            {
+                shield -= damage;
+
+                if(shield < 0)
+                {
+                    shield = 0;
+                }
+            } else
+            {
+                hitPoints -= damage;
+                
+                if(hitPoints <= 0)
+                {
+                    //Die();
+                }
+            }
+        }
+    }
+}
--- Aula1-fortnite/Aula1-fortnite/Program.cs
+++ Aula1-fortnite/Aula1-fortnite/Program.cs
@@ -10,6 +10,16 @@ namespace Aula1_fortnite
     {
         static void Main(string[] args)
         {
+            FHPlayer player1 = new FHPlayer();
+            FHPlayer player2 = new FHPlayer("machineGun", 100, 100);
+
+            Console.WriteLine("Shield before: " + player2.shield);
+            Console.WriteLine("Hit points before: " + player2.hitPoints);
+
+            player1.Attack(player2);
+
+            Console.WriteLine("Shield after: " + player2.shield);
+            Console.WriteLine("Hit points after: " + player2.hitPoints);
         }
     }
 }
