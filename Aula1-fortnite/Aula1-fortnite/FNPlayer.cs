7ce8e21c757bea49b397e914284d7977aa844df4
ineesgoncalvees <ines98goncalves@gmail.com>
2018-10-02 11:02:59 +0100

    Adicianar projeto

---
+++ Aula1-fortnite/Aula1-fortnite/FNPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1_fortnite
+{
+    class FNPlayer
+    {
+        string equipedWeapon;
+        double hitPoints;
+        double shield;
+
+        void Attack(FNPlayer enemy)
+        {
+            enemy.TakeDamage(10);
+        }
+
+        void TakeDamage(double damage)
+        {
+            if(shield > 0)
+            {
+                shield -= damage;
+
+                if(shield < 0)
+                {
+                    shield = 0;
+                }
+            } else
+            {
+                hitPoints -= damage;
+                
+                if(hitPoints <= 0)
+                {
+                    Die();
+                }
+            }
+        }
+    }
+}
