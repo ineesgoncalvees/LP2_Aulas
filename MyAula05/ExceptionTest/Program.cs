de5f40ba78b1385103d096320b1f647e521584e1
ineesgoncalvees <ines98goncalves@gmail.com>
2018-11-06 15:20:24 +0000

    Exercicio 2

---
+++ MyAula05/ExceptionTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insere um numero inteiro:");
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Numero inserido:{i}");
+        }
+    }
+}
