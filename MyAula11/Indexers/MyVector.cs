231043f2c3aa9931a3974cc0a503d7443dd7e2da
ineesgoncalvees <ines98goncalves@gmail.com>
2018-12-11 15:14:11 +0000

    exercicio 1

---
+++ MyAula11/Indexers/MyVector.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    public struct MyVector
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public MyVector(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+
+        public float this[int index]
+        {
+            get
+            {
+                if (index == 0) { return X; }
+                else if (index == 1) { return Y; }
+                else { throw new IndexOutOfRangeException(); }
+            }
+            set
+            {
+                if (index == 0)
+                    X = value;
+
+                else if (index == 1)
+                    Y = value;
+
+                else { throw new IndexOutOfRangeException(); }
+            }
+        }
+
+        public float this[string index] {
+            get {
+                index = index.ToLower();
+                if (index == "0" || index == "a" || index == "x") { return X; }
+
+                else if (index == "1" || index == "b" || index == "y") { return Y; }
+
+                else { throw new IndexOutOfRangeException(); }
+            }
+
+            set
+            {
+                index = index.ToLower();
+                if (index == "0" || index == "a" || index == "x")
+                    X = value;
+
+                else if (index == "1" || index == "b" || index == "y")
+                    Y = value;
+
+                else { throw new IndexOutOfRangeException(); }
+            }
+        }
+    }
+}
---
+++ MyAula11/Indexers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyVector vetor = new MyVector(1,0);
+
+            Console.WriteLine(vetor.X);
+            Console.WriteLine(vetor.Y);
+
+            vetor[0] = 2;
+            vetor[1] = 2;
+
+            Console.WriteLine(vetor.X);
+            Console.WriteLine(vetor.Y);
+
+            vetor["a"] = 3;
+            vetor["y"] = 3;
+
+            Console.WriteLine(vetor.X);
+            Console.WriteLine(vetor.Y);
+        }
+    }
+}
